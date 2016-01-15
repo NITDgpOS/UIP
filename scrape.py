5d2afc16c07618ae93c782161c39680cffdc65f1
Abhay Raizada <abhsag24@gmail.com>
2016-01-15 11:19:46 +0530

    Add Documentation

--- scrape.py
+++ scrape.py
@@ -3,6 +3,7 @@ from urllib import request
 from urllib.request import urlopen
 from urllib.request import urlretrieve
 import os
+pics_folder ='/pics'
 def make_soup(url):
     req = request.Request(url=url,headers={'User-Agent':' Mozilla/5.0 (Windows NT 6.1; WOW64; rv:12.0) Gecko/20100101 Firefox/12.0'})
     response = request.urlopen(req)
@@ -12,22 +13,26 @@ def make_soup(url):
     return BeautifulSoup(html)
 
 def get_images(url):
-    soup = make_soup(url)
+    soup = make_soup(url) 
     #this makes a list of bs4 element tags
-
     thumbnails = soup.find_all("a",class_="thumbnail",href=True)
+    
+    """Thumbnails in /r/wallpapers contain href to original 
+    full-sized image."""
+
     image_links=[]
     if not thumbnails:
         print('No matching image found')
         return
+
     for link in thumbnails:
         if link['href'].endswith(('jpg','png','jpeg')):
             image_links.append( link['href'])
-    print(image_links)
+    
     for image in image_links:
-        #filename.append(image.split('/')[-1])
-        path=os.getcwd()+'/pics'
-        print (path)
+        if(len(image_links)==15):
+            break
+        path=os.getcwd()+ pics_folder
         urlretrieve(image,os.path.join(path,image.split('/')[-1]))
     
 
@@ -35,5 +40,3 @@ try:
     get_images("https://www.reddit.com/r/wallpapers");
 except ValueError as e: 
     print("File could not be retrieved.", e)
-else:
-    print("It worked!")
