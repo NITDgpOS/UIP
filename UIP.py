84f529d40b02a38a63ed5e39d7782e90b713aca2
vishva_nath <vishvanath45@github.com>
2016-11-16 19:41:06 +0530

    Fixed issues related to welcome message

--- UIP.py
+++ UIP.py
@@ -1,9 +1,10 @@
 from scheduler import scheduler
-
+    
 if __name__ == "__main__":
+  print("Hey this is UIP! you can use it to download images from reddit and also to schedule the setting of these images as your desktop wallpaper.")
   try:
     print("Press 1 to connect to internet or any other key to use this application in offline mode")
-    offline = int(input()) != 1
+    offline=int(input())!=1
     scheduler(offline)
   except KeyboardInterrupt:
     import sys
