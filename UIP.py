a406fb4462fb924d491375f502bd052d3e34ac39
DarkSouL <sundeep.sundeep11@gmail.com>
2016-11-16 10:58:27 +0530

    Issue #11 Added option for not connecting to the internet

--- UIP.py
+++ UIP.py
@@ -1,4 +1,6 @@
 from scheduler import scheduler
 
 if __name__ == "__main__":
-	scheduler()
+  print("Press 1 to connect to internet or any other key to use this application in offline mode")
+  offline = int(input()) != 1
+  scheduler(offline)
