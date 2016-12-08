b4fc6a63cfbc6d1b28513905d54a856b53856350
Naveen Kumar Sangi <nkprince007@gmail.com>
2016-12-08 18:46:07 +0530

    Lint with PEP8Bear

--- uiplib/UIP.py
+++ uiplib/UIP.py
@@ -1,7 +1,7 @@
 import sys
 import os
 import shutil
-from uiplib.settings import ParseSettings
+from uiplib.settings import ParseSettings, HOME_DIR
 from uiplib.scheduler import scheduler
 from uiplib.utils import make_dir
 from daemoniker import Daemonizer, send, SIGTERM
